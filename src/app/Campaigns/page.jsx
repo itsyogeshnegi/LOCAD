e1661827b945d3f763bd1fdc80f2e7eeea32f4d7
yogesh <yogesh@locad.net>
2024-07-24 13:41:01 +0530

    done

--- src/app/Campaigns/page.jsx
+++ src/app/Campaigns/page.jsx
@@ -26,12 +26,15 @@ const page = () => {
         <img src="/campaign-measurement/s1.png" />
       </div>
       <div className="flex justify-center items-center">
-        <h1 className="text-2xl">Key Features</h1>
+        <h1 className="text-2xl font-bold">Key Features</h1>
       </div>
       <div>
         <ul className="px-14">
           <li>
-            {`1. Advanced Tracking Technologies: Leverage the latest in tracking
+            <span className="font-semibold">
+              1.Advanced Tracking Technologies:{" "}
+            </span>
+            {`Leverage the latest in tracking
             technology to monitor the effectiveness of your OOH and pDOOH
             campaigns. Our system captures real-time data on ad visibility,
             engagement, and audience interaction, providing you with a clear
@@ -39,7 +42,29 @@ const page = () => {
           </li>
           <br />
           <li>
-            {`2. Comprehensive Attribution Models: Understand the true value of
+            <span className="font-semibold">2. ROI Measurement:</span>
+            {` Accurately measure the return on investment for your OOH and pDOOH campaigns. Our tools calculate the financial impact of your advertising efforts, helping you understand which campaigns deliver the best results and where to allocate future budgets.
+`}
+          </li>
+
+          <br />
+          <li>
+            <span className="font-semibold">
+              3. Detailed Performance Analytics:
+            </span>
+            {` Access in-depth analytics that break down key performance indicators (KPIs) such as impressions, dwell time, engagement rates, and conversion metrics. Our intuitive dashboard allows you to easily interpret data and make informed decisions.`}
+          </li>
+          <br />
+          <li>
+            <span className="font-semibold">4. Real-Time Reporting:</span>
+            {` Benefit from real-time reporting capabilities that provide instant insights into your campaign’s performance. Stay updated with live data feeds and customizable reports that keep you informed and agile.`}
+          </li>
+          <br />
+          <li>
+            <span className="font-semibold">
+              5. Comprehensive Attribution Models:
+            </span>
+            {`Understand the true value of
             your OOH and pDOOH campaigns with our multi-touch attribution
             models. Track how each ad exposure contributes to the customer
             journey and overall conversion, giving you a holistic view of your
@@ -47,26 +72,15 @@ const page = () => {
           </li>
           <br />
           <li>
-            {`3. Detailed Performance Analytics: Access in-depth analytics that break down key performance indicators (KPIs) such as impressions, dwell time, engagement rates, and conversion metrics. Our intuitive dashboard allows you to easily interpret data and make informed decisions.`}
+            <span className="font-semibold">6. Continuous Optimization:</span>
+            {` Optimize your campaigns on-the-fly with our continuous improvement features. Use real-time data and performance insights to adjust your strategies, ensuring that your campaigns remain effective and responsive to changing market conditions.`}
           </li>
           <br />
           <li>
-            {`4. Real-Time Reporting: Benefit from real-time reporting capabilities that provide instant insights into your campaign’s performance. Stay updated with live data feeds and customizable reports that keep you informed and agile.
-`}
-          </li>
-          <br />
-          <li>
-            {`5. ROI Measurement: Accurately measure the return on investment for your OOH and pDOOH campaigns. Our tools calculate the financial impact of your advertising efforts, helping you understand which campaigns deliver the best results and where to allocate future budgets.
-`}
-          </li>
-          <br />
-          <li>
-            {`6. Continuous Optimization: Optimize your campaigns on-the-fly with our continuous improvement features. Use real-time data and performance insights to adjust your strategies, ensuring that your campaigns remain effective and responsive to changing market conditions.`}
-          </li>
-          <br />
-          <li>
-            {`7. Pre-Post Campaign Measurement: Get both pre and post campaign measurement to better understand the location visitation attribution. Our platform not only helps to identify the audience devices across locations but also re-target audiences anytime in future as well via their device ids. 
-`}
+            <span className="font-semibold">
+              7. Pre-Post Campaign Measurement:
+            </span>
+            {` Get both pre and post campaign measurement to better understand the location visitation attribution. Our platform not only helps to identify the audience devices across locations but also re-target audiences anytime in future as well via their device ids. `}
           </li>
         </ul>
       </div>
@@ -76,15 +90,22 @@ const page = () => {
         <img src="/campaign-measurement/s4.png" />
       </div>
       <div className="flex text-center justify-center items-center flex-col px-10">
-        <h1>Why Choose Us?</h1>
+        <h1 className="font-semibold">Why Choose Us?</h1>
       </div>
       <div className="px-14">
         <li>
-          {`Expertise: Benefit from our extensive experience (10yrs+) and expertise in OOH and pDOOH advertising, ensuring your campaigns are designed and measured for success.`}
+          <span className="font-semibold">Expertise:</span>
+          {` Benefit from our extensive experience (10yrs+) and expertise in OOH and pDOOH advertising, ensuring your campaigns are designed and measured for success.`}
         </li>
-        <li>{`Innovation: Utilize the latest technology and innovative solutions to stay ahead of the competition.`}</li>
-        <li>{`Transparency: Enjoy complete transparency with clear, comprehensive reports and actionable insights.`}</li>
-        <li>{`Support: Rely on our dedicated support team to help you interpret data, optimize campaigns, and achieve your marketing objectives.`}</li>
+        <li>
+        <span className="font-semibold">Innovation:</span>
+          {` Utilize the latest technology and innovative solutions to stay ahead of the competition.`}</li>
+        <li>
+        <span className="font-semibold">Transparency:</span>
+          {` Enjoy complete transparency with clear, comprehensive reports and actionable insights.`}</li>
+        <li>
+        <span className="font-semibold">Support:</span>
+          {` Rely on our dedicated support team to help you interpret data, optimize campaigns, and achieve your marketing objectives.`}</li>
       </div>
       {/* <br/> */}
       <div className="px-14 py-4 font-semibold">
--- src/app/TransitMedia/page.jsx
+++ src/app/TransitMedia/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Footer from "@/Components/Footer/Footer";
 import MainNavBar from "@/Components/NavBar/MainNavBar";
 import UpperNav from "@/Components/NavBar/UpperNav";
 import React from "react";
@@ -8,12 +9,15 @@ const page = () => {
     <>
       <UpperNav />
       <MainNavBar />
-      <div className="p-2 flex justify-center items-center">
+      <div className="p-1 flex justify-center items-center flex-col">
+        <p className="text-xl font-semibold">
+          New Age Transit Advertising Platform with Tracking and Live Progress
+        </p>
         <h1>Transit Media</h1>
       </div>
       <div className="px-10 py-1">
         <p className="text-justify">
-          {` New Age Transit Advertising Platform with Tracking and Live Progress
+          {`
           Introducing our cutting-edge Transit Advertising Platform, designed to
           revolutionize the way brands engage with their audiences on the move.
           Whether its cab, auto, train or aircraft branding our platform
@@ -25,57 +29,69 @@ const page = () => {
       <div className="text-center text-2xl font-semibold">
         <p>Why Choose Our Transit Advertising Platform?</p>
       </div>
-      <div className="flex flex-col text-center items-center w-full justify-center px-5">
+      <div className="flex justify-center items-center w-full">
+      <div className="flex flex-col  ">
         <li className="text-start">
-          Innovation: Stay ahead of the competition with our innovative tracking
-          and monitoring technology.
+          <span className="font-semibold">Innovation:</span> Stay ahead of the
+          competition with our innovative tracking and monitoring technology.
         </li>
         <li className="text-start">
-          Efficiency: Optimize your campaigns with real-time data and actionable
-          insights.
+          <span className="font-semibold">Efficiency:</span> Optimize your
+          campaigns with real-time data and actionable insights.
         </li>
         <li className="text-start">
-          Transparency: Enjoy complete transparency with detailed,
-          easy-to-understand reports.
+          <span className="font-semibold">Transparency:</span> Enjoy complete
+          transparency with detailed, easy-to-understand reports.
         </li>
         <li className="text-start">
-          Support: Benefit from our dedicated support team, ready to assist you
-          at every step.
+          <span className="font-semibold">Support:</span> Benefit from our
+          dedicated support team, ready to assist you at every step.
         </li>
       </div>
+      </div>
+
       <div className="flex flex-col text-center items-center w-full justify-center px-5 mt-5">
-        <h1 className="text-xl  py-1 font-bold">
-          PLATFORM SCREENSHOTS AND DRIVER APP SCREENSHOTS ALSO + PICTURES OF
-          TRANSIT MEDIA CAMPAIGNS
-        </h1>
-        <p className="text-xl font-Teko">Key Features</p>
+        <p className="text-3xl font-bold">Key Features</p>
         <p className="text-start">
-          1. Advanced Tracking Capabilities: Our platform leverages
-          state-of-the-art tracking technology to monitor your transit ads in
-          real-time. Whether your advertisements are on buses, taxis, or trains,
-          we provide comprehensive data on ad visibility and audience
-          engagement.
+          <span className="font-semibold">1. Advanced</span> Tracking
+          Capabilities: Our platform leverages state-of-the-art tracking
+          technology to monitor your transit ads in real-time. Whether your
+          advertisements are on buses, taxis, or trains, we provide
+          comprehensive data on ad visibility and audience engagement.
         </p>
         <p className="text-start">
-          2. Live Progress Monitoring: Stay updated with the live progress of
-          your campaigns. Our dashboard offers real-time insights into the reach
-          and effectiveness of your ads, allowing you to make informed decisions
-          and adjustments on the fly.
+          <span className="font-semibold">2. Live RTO Verification:</span>
+          {` With our platform you can now for the 1st
+          time in India get RTO verification on the fly to ensure greater
+          transparency and accountability. No way for local installers to fudge
+          or duplicate pictures!!`}
         </p>
         <p className="text-start">
-          {`3. Detailed Analytics and Reporting: Access in-depth analytics that
+          <span className="font-semibold">3. Live Progress Monitoring:</span>{" "}
+          Stay updated with the live progress of your campaigns. Our dashboard
+          offers real-time insights into the reach and effectiveness of your
+          ads, allowing you to make informed decisions and adjustments on the
+          fly.
+        </p>
+        <p className="text-start">
+          <span className="font-semibold">
+            4. Detailed Analytics and Reporting:
+          </span>
+          {` Access in-depth analytics that
           provide a clear picture of your campaign’s performance. Our reports
           include key metrics such as impressions, engagement rates, and
           conversion data, helping you understand the impact of your advertising
           efforts.`}
         </p>
         <p className="text-start">
-          {`4. Driver Engagement & Pay-out: Platform seamlessly makes direct
+          <span className="font-semibold">5. Driver Engagement & Pay-out:</span>
+          {` Platform seamlessly makes direct
           payment to drivers of the vehicles via the Driver’s app. Get real time
           visibility and on-demand proof of posting of your campaigns.`}
         </p>
         <p className="text-start">
-          {`5. Customizable Campaigns: Design and launch campaigns that cater to
+          <span className="font-semibold">6. Customizable Campaigns:</span>
+          {` Design and launch campaigns that cater to
           your specific needs and objectives. Our platform offers a variety of
           customization options, ensuring that your ads align perfectly with
           your brand’s message and goals. Transform your transit advertising
@@ -84,6 +100,7 @@ const page = () => {
           engagement and achieve your marketing goals with precision and ease.`}
         </p>
       </div>
+      <Footer/>
     </>
   );
 };
