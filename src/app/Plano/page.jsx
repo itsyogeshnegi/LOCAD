d343cc5a91bff3483ca122ddace736f8b53a18b7
yogesh <yogesh@locad.net>
2024-07-31 14:03:01 +0530

    done

--- src/app/Plano/page.jsx
+++ src/app/Plano/page.jsx
@@ -44,14 +44,12 @@ const page = () => {
           width: "100%",
           fontWeight: "900",
         }}>
-        <h3>Key Features:</h3>
+        <h3 className="font-bold">Key Features</h3>
       </div>
       <div className="planoFeatures">
         <div className="plano_box">
           <ol start={1}>
-            <p style={{ fontWeight: "700", fontSize: "15px" }}>
-              Inventory Management:
-            </p>
+            <p className="text-xl font-semibold">Inventory Management</p>
             <li>
               1.Comprehensive Database: Access a centralized repository for all
               your site locations.
@@ -68,26 +66,33 @@ const page = () => {
         </div>
         <div className="plano_box">
           <ol start={1}>
-            <p style={{ fontWeight: "700" }}>Data Driven Campaign Planning:</p>
+            <p className="text-xl font-semibold">
+              Data Driven Campaign Planning
+            </p>
             <li>
-              1.Targeting and Segmentation: Select sites based on demographic, geographic, and psychographic criteria.
+              1.Targeting and Segmentation: Select sites based on demographic,
+              geographic, and psychographic criteria.
             </li>
             <li>
-              2.Budget Allocation: Manage your budget efficiently across different locations.
+              2.Budget Allocation: Manage your budget efficiently across
+              different locations.
             </li>
             <li>
-             3.Performance Forecasting: Use predictive analytics to estimate the impact and reach of your campaigns.
+              3.Performance Forecasting: Use predictive analytics to estimate
+              the impact and reach of your campaigns.
             </li>
           </ol>
         </div>
         <div className="plano_box">
           <ol start={1}>
-            <p style={{ fontWeight: "700" }}>Sales and Booking</p>
+            <p className="text-xl font-semibold">Sales and Booking</p>
             <li>
-            1.Automated Sales Processes: Streamline workflows for booking and sales, reducing manual effort.
+              1.Automated Sales Processes: Streamline workflows for booking and
+              sales, reducing manual effort.
             </li>
             <li>
-              2.Client Portal: Provide clients with a dedicated portal to view available sites, book spaces, and track campaign progress.
+              2.Client Portal: Provide clients with a dedicated portal to view
+              available sites, book spaces, and track campaign progress.
             </li>
             <li>
               Historical performance data to inform media selection decisions.
@@ -96,15 +101,18 @@ const page = () => {
         </div>
         <div className="plano_box">
           <ol start={1}>
-            <p style={{ fontWeight: "700" }}>Reporting and Analytics:</p>
+            <p className="text-xl font-semibold">Reporting and Analytics</p>
             <li>
-              1.Detailed Reports: Generate customizable reports on inventory status, campaign performance, and financials.
+              1.Detailed Reports: Generate customizable reports on inventory
+              status, campaign performance, and financials.
             </li>
             <li>
-              2.Data Integration: Integrate with other data sources and platforms for comprehensive analysis.
+              2.Data Integration: Integrate with other data sources and
+              platforms for comprehensive analysis.
             </li>
             <li>
-              3.Dashboards: Monitor real-time data and gain insights through interactive dashboards.
+              3.Dashboards: Monitor real-time data and gain insights through
+              interactive dashboards.
             </li>
           </ol>
         </div>
@@ -133,48 +141,74 @@ const page = () => {
       <div className="w-full px-2 justify-center items-center flex">
         <ul className="px-3 ">
           <li className="">
-          <span className="font-bold">Efficiency:</span><br/>
-          Time-Saving: Automated processes reduce the time required for inventory management and campaign planning.<br/>
-          Reduced Errors: Minimize human errors with automated data entry and management.
-
+            <span className="font-bold text-xl">Efficiency</span>
+            <br />
+            Time-Saving Automated processes reduce the time required for
+            inventory management and campaign planning.
+            <br />
+            Reduced Errors: Minimize human errors with automated data entry and
+            management.
           </li>
-          <br/>
+          <br />
           <li>
-          <span className="font-bold">Scalability:</span><br/>
-          Flexible: Easily scalable to manage an increasing number of sites and campaigns.<br/>
-          Adaptable: Suitable for small vendors with a few sites to large vendors managing thousands of locations.
-
+            <span className="font-bold text-xl">Scalability</span>
+            <br />
+            Flexible Easily scalable to manage an increasing number of sites
+            and campaigns.
+            <br />
+            Adaptable: Suitable for small vendors with a few sites to large
+            vendors managing thousands of locations.
           </li>
-          <br/>
+          <br />
           <li>
-          <span className="font-bold">Improved Decision Making:</span><br/>
-          Data-Driven Insights: Access comprehensive data to make informed decisions.<br/>
-          Performance Tracking: Continuously monitor and optimize campaigns with detailed reporting.
-
+            <span className="font-bold text-xl">Improved Decision Making</span>
+            <br />
+            Data-Driven Insights: Access comprehensive data to make informed
+            decisions.
+            <br />
+            Performance Tracking: Continuously monitor and optimize campaigns
+            with detailed reporting.
           </li>
-          <br/>
+          <br />
           <li>
-          <span className="font-bold">Enhanced Client Satisfaction:</span><br/>
-          Transparency: Clients can view available inventory and campaign progress, improving trust and satisfaction.<br/>
-          Responsive Service: Faster response times due to streamlined processes and real-time updates.
-
+            <span className="font-bold text-xl">Enhanced Client Satisfaction</span>
+            <br />
+            Transparency Clients can view available inventory and campaign
+            progress, improving trust and satisfaction.
+            <br />
+            Responsive Service: Faster response times due to streamlined
+            processes and real-time updates.
           </li>
-          <br/>
+          <br />
           <li>
-          <span className="font-bold">Use Cases:</span><br/>
-          Local OOH Vendors: Simplify the management of a few sites within a city or region.<br/>
-          National OOH Networks: Efficiently manage extensive inventories spread across the country.<br/>
-          Advertising Agencies: Plan, book, and monitor campaigns for your clients with ease.<br/>
-          Large Corporations: Manage internal advertising assets and campaigns across various locations.
-
+            <span className="font-bold text-xl">Use Cases</span>
+            <br />
+            Local OOH Vendors Simplify the management of a few sites within a
+            city or region.
+            <br />
+            National OOH Networks: Efficiently manage extensive inventories
+            spread across the country.
+            <br />
+            Advertising Agencies: Plan, book, and monitor campaigns for your
+            clients with ease.
+            <br />
+            Large Corporations: Manage internal advertising assets and campaigns
+            across various locations.
           </li>
         </ul>
       </div>
       <div className="flex w-full flex-col px-2 justify-center items-center my-3">
         <h1 className="font-semibold">Why Choose PLANO?</h1>
-        <p className="mx-7 text-justify">PLANO is designed to meet the diverse needs of OOH site vendors, offering a robust platform that ensures better resource utilization, improved campaign performance, and enhanced client satisfaction. Simplify your OOH media planning with PLANO and experience the future of inventory management and campaign optimization.</p>
+        <p className="mx-7 text-justify">
+          PLANO is designed to meet the diverse needs of OOH site vendors,
+          offering a robust platform that ensures better resource utilization,
+          improved campaign performance, and enhanced client satisfaction.
+          <br/>
+          Simplify your OOH media planning with PLANO and experience the future of inventory management and campaign optimization.
+        </p>
+        {/* <p className="mx-7 text-justify">{` `}</p> */}
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
